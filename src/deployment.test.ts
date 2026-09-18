bba927dcfc805eaf753fab47bff2ba5d2f5a1236
simply-sunny <saunak.karnati@gmail.com>
2026-09-18 15:52:41 -0400

    chore: remove published site artifacts

--- src/deployment.test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, it } from 'vitest'
-import config from '../vite.config'
-
-it('uses the GitHub Pages repository base', () => {
-  expect(config).toMatchObject({ base: '/sunny-components/' })
-})
